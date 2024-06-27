71286dfc02d580827ab352680fd61dbef183fea9
arun8270 <rdarunkumarcs@gmail.com>
2024-06-27 09:18:22 +0530

    created accordian

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 
-function FormulaItem() {
+let formulas = [{ id : 1, question : 'Total Fare', formula : 'Initial Fare(Base Fare) + KM Fare + Travel Time Fare '} ,{id : 2, question : 'GST on Total Fare',
+formula : '(Initial Fare + KM fare + Travel Time Fare) * GST %'} , {id : 3, question : 'Net Fare', formula : 'totalFare + GST'} ,
+  {id : 4, question : 'Grand Total', formula : 'netFare + Booking Convenience Fee + Booking Convenience Fee CGST + Booking Convenience Fee + Booking Convenience Fee SGST'
+}]
+
+
+function FormulaItem({question, answer}) {
+  const [show, setShow] = useState(false);
+  const toggleShow = ()=>{
+    setShow(!show)
+  }
   return (
-    <div className="formula-item">
-      <div className="formula-header">Actual Fare Calculation Explanation</div>
+    <div className={`formula-item ${show ? 'active' : ''}`}>
+      <div className="formula-header" onClick={toggleShow}>{question}</div>
       <div className = 'formula-body'>
       <div className="formula-body-content">
-        Actual Fare = Initial Fare + KM Fare + Travel Time Fare + GST(Initial
-        Fare + KM Fare + Travel Time Fare) + Booking Convenience Fee + Booking
-        Convenience Fee CGST + Booking Convenience Fee SGST
+        {answer}
       </div>
       </div>
       
@@ -17,14 +25,11 @@ function FormulaItem() {
   );
 }
 
-function FormulaAccordian() {
+function FormulaAccordian({formulas}) {
   return (
     <div className="formula-accordian">
       <h2>Fare Formula's</h2>
-      <FormulaItem />
-      <FormulaItem />
-      <FormulaItem />
-      <FormulaItem />
+      {formulas.map((item => (<FormulaItem key = {item.id} question = {item.question} answer = {item.formula} />)))}
     </div>
   );
 }
@@ -33,7 +38,7 @@ function App() {
 
   return (
     <>
-      <FormulaAccordian />
+      <FormulaAccordian formulas = {formulas} />
     </>
   );
 }
