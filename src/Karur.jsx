f6f57c28a88a8d8a98475741c5ba5ba06e0509d2
arun8270 <rdarunkumarcs@gmail.com>
2024-07-27 08:21:23 +0530

    Latest

--- src/Karur.jsx
+++ src/Karur.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './Karur.css'
  function KarurSlot(){
     const[fareslot, setFareSlot] = useState({initialFreeKM : 2, initialFare : 105, farePerKM : 15, kmLimit : 5, kmAfterLimit : 17, freeWaitingMinute : 0, waitingFarePerminute : 2, rideChargePerMinute : 0, emptyKMCharge : 10});
     function handleSubmit(e){
@@ -18,17 +19,43 @@ import React, { useState } from "react";
         <>
         <form onSubmit={handleSubmit}>
             <div className="slotinput-container">
-            <input type="number" name="freekm" placeholder="Initial Free KM" value={fareslot.initialFreeKM} onChange={handleChange}/>
-            <input type="number" name="initialFare" placeholder="Initial Fare" value={fareslot.initialFare} onChange={handleChange}/>
-            <input type="number" name="farePerKM" placeholder="Fare Per KM" value={fareslot.farePerKM} onChange={handleChange}/>
-            <input type="number" name="kmLimit" placeholder="KM Limit" value={fareslot.kmLimit} onChange={handleChange}/>
-            <input type="number" name="afterLimitFare" placeholder="Fare/KM after KM Limit" value={fareslot.kmAfterLimit} onChange={handleChange}/>
-            <input type="number" name="freewaitingMinute" placeholder="Free Waiting Minute" value={fareslot.freeWaitingMinute} onChange={handleChange}/>
-            <input type="number" name="waitingFarePerMinute" placeholder="Waiting Fare Per Minute" value={fareslot.waitingFarePerminute} onChange={handleChange}/>
-            <input type="number" name="rideChargePerMinute" placeholder="Ride Charge/Min" value={fareslot.rideChargePerMinute} onChange={handleChange}/>
-            <input type="number" name="emptyKMCharge" placeholder="Empty Km Charge" value={fareslot.emptyKMCharge} onChange={handleChange}/>
+            <div>
+            <label htmlFor="freekm">Initial Free KM : </label>
+            <input type="number" id="freekm" name="initialFreeKM" placeholder="Initial Free KM" value={fareslot.initialFreeKM} onChange={handleChange}/>
             </div>
-            <button type="submit">Save Changes</button>
+            <div>
+            <label htmlFor="initialfare">Initial Fare : </label>
+            <input type="number" id="initialfare" name="initialFare" placeholder="Initial Fare" value={fareslot.initialFare} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="fareperkm">Fare Per KM : </label>
+            <input type="number" id="fareperkm" name="farePerKM" placeholder="Fare Per KM" value={fareslot.farePerKM} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="kmlimit">KM Limit : </label>
+            <input type="number" id="kmlimit" name="kmLimit" placeholder="KM Limit" value={fareslot.kmLimit} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="kmafterlimit">KM After Limit : </label>
+            <input type="number" id="kmafterlimit" name="kmAfterLimit" placeholder="Fare/KM after KM Limit" value={fareslot.kmAfterLimit} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="waitingminute">Free Waiting Minute : </label>
+            <input type="number" id="waitingminute" name="freeWaitingMinute" placeholder="Free Waiting Minute" value={fareslot.freeWaitingMinute} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="waitingfareperminute">Waiting Fare Per Minute : </label>
+            <input type="number" id="waitingfareperminute" name="waitingFarePerminute" placeholder="Waiting Fare Per Minute" value={fareslot.waitingFarePerminute} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="fareperminute">Ride Charge Per Minute : </label>
+            <input type="number" id="fareperminute" name="rideChargePerMinute" placeholder="Ride Charge/Min" value={fareslot.rideChargePerMinute} onChange={handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="emptykmcharge">Empty KM Charge Per KM : </label>
+            <input type="number" id="emptykmcharge" name="emptyKMCharge" placeholder="Empty Km Charge" value={fareslot.emptyKMCharge} onChange={handleChange}/>
+            </div>
+            </div>
         </form>
         </>
     )
